adfb7174e4c89a50d34d6d5b8414b6b9e55f0d65
Adarsha <paudeladarsha111@gmail.com>
2024-10-29 23:43:39 +0000

    .

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import productRoutes from './routes/product.route.js';
-import emailRoutes from './routes/email.route.js';
 
 dotenv.config();
 
 const app = express();
 
+// Define allowed origins
+const allowedOrigins = [process.env.CORS_ORIGIN, 'http://localhost:5173/']; // Add any other allowed origins here
+
+// Configure CORS to allow multiple origins
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: (origin, callback) => {
+        // Allow requests with no origin, like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 
@@ -19,8 +29,11 @@ app.use(express.urlencoded({ extended: true, limit: '16mb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-// Use routes
-app.use('/api/v1/product', productRoutes); // This should match your route path
+// Routes
+import productRoutes from './routes/product.route.js';
+import emailRoutes from './routes/email.route.js';
+
+app.use('/api/v1/product', productRoutes);
 app.use('/api/v1/email', emailRoutes);
 
 app.get('/', (req, res) => {
